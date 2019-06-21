5b780d5d356bff3d30f51269443b203789ae1207
isadorabnunes <isadora.nunes@novaandradina.org>
2019-06-21 11:14:49 -0300

    back

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const cors = require('cors')
+const mongoose = require('mongoose');
+const requireDir = require('require-dir');
+
+const app = express();
+app.use(express.json());
+app.use(cors()) 
+
+mongoose
+  .connect(
+    'mongodb+srv://isadora:cartnote@cluster0-kcs6o.mongodb.net/test?retryWrites=true&w=majority',
+    {
+      useNewUrlParser: true
+    })
+  .then(() => console.log('MongoDB Connected'))
+  .catch(err => console.log(err));
+
+requireDir('./src/models');
+
+app.use('/api', require('./src/routes.js'));
+
+app.listen(3000);
