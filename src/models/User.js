6aea187b015662c5db8e24aa0b4ee0ca8f530fb5
willian <willian.lourenco@novaandradina.org>
2019-06-24 10:18:05 -0400

    fixing some bugs

--- src/models/User.js
+++ src/models/User.js
@@ -1,9 +1,18 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
 const UserSchema = new mongoose.Schema({
-    id: { type: Number, required: true },
-    name: { type: String, required: true },
-    password: { type: String, required: true },
-    productLists: [ProductsList.schema]
+	id: {
+		type: Number,
+		required: true
+	},
+	name: {
+		type: String,
+		required: true
+	},
+	password: {
+		type: String,
+		required: true
+	}
+});
 
-mongoose.model('User', UserSchema);
+mongoose.model("User", UserSchema);
